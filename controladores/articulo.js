fd63ada8def52fb5ddb0acbf42f7f64710b25860
juliop11 <julioperezobrero@hotmail.com>
2023-06-14 18:42:51 +0200

    Comenzando curso y blog

--- controladores/articulo.js
+++ controladores/articulo.js
@@ -1,4 +1,6 @@
-const {validarArticulo} = require("../helpers/validar");
+const fs = require("fs");
+const path = require("path");
+const { validarArticulo } = require("../helpers/validar");
 const Articulo = require("../modelos/Articulo");
 
 
@@ -167,7 +169,76 @@ const editar = (req, res) => {
                 })
             }
         })
-
-
 }
-module.exports = { prueba, curso, crear, listar, mostrarUno, borrar, editar }
+
+const subir = (req, res) => {
+
+    //Configurar multer
+
+    //Recoger el fichero de imagen subido
+    if (!req.file && !req.files) {
+        return res.status(404).json({
+            status: "error",
+            mensaje: "Peticion invalida"
+        })
+    }
+    //Nombre del archivo
+    let archivo = req.file.originalname;
+
+    //Extension del archivo
+    let archivo_split = archivo.split("\.");
+    let extension = archivo_split[1];
+
+    //Comprobar extension correcta
+    if (extension != "png" && extension != "jpg" && extension != "jpeg" && extension != "gif") {
+
+        //Borrar archivo de carpeta imagenes y dar respuesta
+        fs.unlink(req.file.path, (error) => {
+            return res.status(400).json({
+                status: "error",
+                mensaje: "Imagen invalida"
+            });
+        })
+    } else {
+        //Recoger id articulo a editar
+        let articuloId = req.params.id;
+
+        //Buscar y actualizar articulo
+        Articulo.findOneAndUpdate({ _id: articuloId }, { imagen: req.file.filename }, { new: true })
+
+            //Devolver respuesta
+            .then((articuloActualizado) => {
+                return res.status(200).json({
+                    status: "succes",
+                    articulo: articuloActualizado,
+                    fichero: req.file
+                });
+            })
+            .catch((error) => {
+                if (error || !articuloActualizado) {
+                    return res.status(500).json({
+                        status: "error",
+                        mensaje: "Error al actualizar el articulo"
+                    })
+                }
+            })
+    }
+}
+//Poniendo la url de la imagen nos la muestra
+const imagen = (req, res) => {
+    let fichero = req.params.fichero;
+    let ruta_fisica = "./imagenes/articulos/" + fichero;
+
+    fs.stat(ruta_fisica, (error, existe) => {
+        if (existe) {
+            return res.sendFile(path.resolve(ruta_fisica));
+        }
+        else {
+            return res.status(404).json({
+                status: "error",
+                mensaje: "La imagen no existe"
+            })
+        }
+    })
+}
+module.exports = { prueba, curso, crear, listar, mostrarUno, borrar, editar, subir, imagen }
--- rutas/articulo.js
+++ rutas/articulo.js
@@ -1,7 +1,20 @@
 const express = require("express");
-const router = express.Router();
+const multer = require("multer");
+const articuloControlador = require("../controladores/articulo");
 
-const articuloControlador = require("../controladores/articulo")
+const router = express.Router();
+//Configurar multar
+const almacenamiento = multer.diskStorage({
+    destination: function(req, file, cb)  {
+        cb(null, './imagenes/articulos/');
+    },
+
+    filename: function(req, file, cb) {
+        cb(null, "articulo" + Date.now() + file.originalname)
+    }
+})
+
+const subidas = multer({ storage: almacenamiento });
 
 //Rutas de prueba
 router.get("/ruta-de-prueba", articuloControlador.prueba)
@@ -14,5 +27,8 @@ router.get("/articulos/:ultimos?", articuloControlador.listar)
 router.get("/articulo/:id", articuloControlador.mostrarUno)
 router.delete("/articulo/:id", articuloControlador.borrar)
 router.put("/articulo/:id", articuloControlador.editar)
+router.post("/subir-imagen/:id", [subidas.single("file0")], articuloControlador.subir)
+router.get("/imagen/:fichero", articuloControlador.imagen)
+
 
 module.exports = router;
